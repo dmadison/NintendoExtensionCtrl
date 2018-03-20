4c1874b7c5b2b583e243b99e2047bcf2201ea7f5
David Madison <dmadison@users.noreply.github.com>
2018-03-20 14:45:33 -0400

    License (#1)
    * Added license
    
    * Added license notices to source
    
    * Added license description to README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Nintendo Extension Controller Library
+## License
+This library is licensed under the terms of the [GNU Lesser General Public License (LGPL)](https://www.gnu.org/licenses/lgpl.html), either version 3 of the License, or (at your option) any later version.
--- src/ExtensionController.h
+++ src/ExtensionController.h
@@ -2,7 +2,22 @@
 *  Project     Nintendo Extension Controller Library
 *  @author     David Madison
 *  @link       github.com/dmadison/NintendoExtensionCtrl
-*  @license    Copyright (c) 2018 David Madison
+*  @license    LGPLv3 - Copyright (c) 2018 David Madison
+*
+*  This file is part of the Nintendo Extension Controller Library.
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef ExtensionController_h

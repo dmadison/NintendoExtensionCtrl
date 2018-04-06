cda511a8b9bcfdcef3e0101dbb7924aea34328aa
David Madison <dmadison@users.noreply.github.com>
2018-04-06 01:32:10 -0400

    Switch I2C libraries for Teensy

--- src/ExtensionController.h
+++ src/ExtensionController.h
@@ -24,7 +24,13 @@
 #define NXC_ExtensionController_h
 
 #include "Arduino.h"
-#include "Wire.h"
+
+#if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MKL26Z64__) || \
+    defined(__MK64FX512__) || defined(__MK66FX1M0__) // Teensy 3.0/3.1-3.2/LC/3.5/3.6
+#include "i2c_t3.h"
+#else
+#include "Wire.h"  // All other platforms
+#endif
 
 enum NXC_ControllerType {
 	NXC_NoController,

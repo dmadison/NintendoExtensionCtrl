a189e83c6080110aceb2e772eb9ccbfc7071c22c
David Madison <dmadison@users.noreply.github.com>
2018-06-10 00:11:10 -0400

    Updated MultipleTypes example with new process

--- examples/Any/MultipleTypes/MultipleTypes.ino
+++ examples/Any/MultipleTypes/MultipleTypes.ino
@@ -1,26 +1,25 @@
 #include <NintendoExtensionCtrl.h>
 
-ExtensionData busData(Wire);  // Shared data for the controller on the 'Wire' bus
-ExtensionController controller(busData);  // Generic type: works with any ID, and requests the max # of bytes possible
+ExtensionController extensionPort;  // Port for communicating with extension controllers
 
-Nunchuk nchuk(busData);
-ClassicController classic(busData);
+Nunchuk::DataMap nchuk(extensionPort);  // Read Nunchuk formatted data from the port
+ClassicController::DataMap classic(extensionPort);  // Read Classic Controller formatted data from the port
 
 void setup() {
 	Serial.begin(115200);
-	controller.begin();
+	extensionPort.begin();
 
-	while (!controller.connect()) {
+	while (!extensionPort.connect()) {
 		Serial.println("No controller found!");
 		delay(1000);
 	}
 }
 
 void loop() {
-	boolean success = controller.update();  // Get new data from the controller
+	boolean success = extensionPort.update();  // Get new data from the controller
 
 	if (success == true) {  // We've got data!
-		NXC_ControllerType conType = controller.getConnectedID();
+		NXC_ControllerType conType = extensionPort.getConnectedID();
 
 		switch (conType) {
 			case(NXC_Nunchuk):

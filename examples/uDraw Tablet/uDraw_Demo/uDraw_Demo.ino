edda22b077547c9368a6a0e960b0d2b0e64f7b89
David Madison <dmadison@users.noreply.github.com>
2020-03-21 15:05:29 -0400

    Rename uDraw_SerialDemo to simply _Demo
    This was "SerialDemo" in contrast to the "MouseDemo" which has since been removed.

--- examples/uDraw Tablet/uDraw_Demo/uDraw_Demo.ino
+++ examples/uDraw Tablet/uDraw_Demo/uDraw_Demo.ino
@@ -19,7 +19,7 @@
 *  You should have received a copy of the GNU Lesser General Public License
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
-*  Example:      uDraw_SerialDemo
+*  Example:      uDraw_Demo
 *  Description:  Connect to uDraw Tablet and demonstrate all of
 *                the avaiable control data functions.
 */
